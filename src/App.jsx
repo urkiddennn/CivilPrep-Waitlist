6546ff7f1d69cf810bbc4cc04f627d70b98f1e56
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-07-01 17:29:59 +0800

    Update: fix the reponsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import RightSplit from "./components/RightSplit";
 function App() {
   return (
     <div
-      className="w-full h-screen flex sm:flex-row flex-col sm:overflow-clip"
+      className="w-full h-screen flex sm:flex-row flex-col sm:overflow-clip overflow-auto "
       data-theme="light"
     >
       <LeftSplit />
