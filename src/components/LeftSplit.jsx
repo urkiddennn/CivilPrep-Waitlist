8d8b89167c8f74524ef04a6cb9cf7450a549234b
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-07-02 19:20:00 +0800

    fixes the layout of the leftsplit

--- src/components/LeftSplit.jsx
+++ src/components/LeftSplit.jsx
@@ -2,10 +2,11 @@ import AvatarStack from "./AvatarStack";
 import InputEmail from "./InputEmail";
 import { IoBookOutline } from "react-icons/io5";
 import { IoMdTime } from "react-icons/io";
+
 const LeftSplit = () => {
     return (
-        <div className="sm:w-1/2 w-full h-full flex flex-col gap-5 justify-start items-start md:px-20 sm:px-10 px-5  md:py-24 sm:py-12 py-6">
-            <div className="flex text-5xl justify-center items-center gap-5">
+        <div className="sm:w-1/2 w-full h-full flex flex-col gap-6 justify-start items-start md:px-20 sm:px-10 px-5 md:py-24 sm:py-12 py-6">
+            <div className="flex text-5xl justify-center items-center gap-5 mb-2">
                 <IoBookOutline className="text-blue-500" />
                 <h1 className="font-extrabold ">CivilPrep</h1>
             </div>
@@ -14,13 +15,15 @@ const LeftSplit = () => {
             <h1 className="sm:text-6xl text-4xl font-semibold my-6">
                 Hello world, I don't have something to say
             </h1>
-            <p className="text-lg text-gray-500">
+            <p className="text-lg text-gray-500 mb-2">
                 Whereas recognition of the inherent dignity
             </p>
-            <InputEmail />
-            <div className="flex flex-col justify-start items-start gap-3 w-full mt-10">
-                <h1 className="text-3xl font-bold">Features</h1>
-                <ul className="w-full flex flex-col gap-5">
+            <div className="w-full flex flex-col items-start">
+                <InputEmail />
+            </div>
+            <div className="flex flex-col justify-start items-start gap-4 w-full mt-12">
+                <h1 className="text-3xl font-bold mb-2">Features</h1>
+                <ul className="w-full flex flex-col gap-6">
                     <li className="w-full flex gap-3">
                         <div className="w-auto flex justify-center items-start float-start gap-3">
                             {/* Unique icon for Design */}
